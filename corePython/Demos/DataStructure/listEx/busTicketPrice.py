7fb456906fca8f18a550e779f332ea5543614efb
dipeshpatidarjeeran <dipeshjeeran@gmail.com>
2025-06-26 19:26:15 +0530

    add list methods and busticketprice_program

--- corePython/Demos/DataStructure/listEx/busTicketPrice.py
+++ corePython/Demos/DataStructure/listEx/busTicketPrice.py
@@ -19,8 +19,30 @@ def busTicketPrice(li,dist,source,dest):
         return ticket_price
 
 li = ['s1','s2','s3','s4','s5','s6']
-dist = [1000,2500,3400,4800,2100,6200]
+dist = [1000,2500,3400,4800,2200,6200]
 source = input("Enter the starting station name(from):-")
 dest = input("Enter thew Ending station name(To):-")
 price = busTicketPrice(li,dist,source,dest)
 print(f"Station {source} to {dest} ticket price is {price} rupees")
+
+
+# li = ['s1','s2','s3','s4','s5','s6']
+# dist = [1000,2500,3400,4800,2200,6200]
+# source = input("Enter the starting station name(from):-")
+# dest = input("Enter thew Ending station name(To):-")
+
+# i_src = li.index(source)
+# i_dest = li.index(dest)
+
+# i = i_src
+# tot_dist = 0
+# while(i != i_dest):
+#     tot_dist += dist[i]
+#     if(i == len(li) - 1):
+#         i = 0
+#     else:
+#         i += 1
+
+# print(tot_dist)
+# tick_price = (tot_dist/1000) * 5
+# print(tick_price)
--- corePython/Demos/DataStructure/listEx/list_methods.py
+++ corePython/Demos/DataStructure/listEx/list_methods.py
@@ -2,8 +2,48 @@ li = [30,40,10,20]
 
 #li.append(50)
 # li.clear()
-li2 = li.copy()
-li3 = li
-li.append(50)
-print(li.index(10))
-print(li2)
+# li2 = li.copy()
+# li3 = li
+# li.append(50)
+# print(li.index(50))
+# print(li2)
+
+# print(li.count(31))
+# print(li.count(10))
+
+# print(li.extend([1,2,3,4]))
+
+# li.insert(0,50)
+# print(li)
+
+# li.pop(0)
+# print(li)
+
+# li.remove(10)
+# print(li)
+
+# li.reverse()
+# print(li)
+
+# li.sort()
+# print(li)
+# li.sort(reverse=True)
+# print(li)
+
+li = [70,80,[10,20,30],[40,50,60]]
+
+# for i in range(len(li)):
+#     for j in range(len(li[i])):
+#         print(li[i][j], end=' ')
+#     print()
+
+sum = 0 
+for sub_list in li:
+    print(sub_list)
+    if type(sub_list) == int:
+        sum += sub_list
+    else:
+        for ele in sub_list:
+            sum += ele
+
+print("addtion:",sum)
