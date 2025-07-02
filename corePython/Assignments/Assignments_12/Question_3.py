50215154ad00b5d743538fea96cc7c189b3d666d
dipeshpatidarjeeran <dipeshjeeran@gmail.com>
2025-07-02 13:50:39 +0530

    add frozenset

--- corePython/Assignments/Assignments_12/Question_3.py
+++ corePython/Assignments/Assignments_12/Question_3.py
@@ -3,7 +3,7 @@ str1 = 'listen'
 str2 = 'silent'
 
 for i in str1:
-    if i not in str2 or (len(str1) != len(str2)):
+    if i not in str2 or (len(str1) != len(str2)) or str1.count(i) != str2.count(i):
         print(f'{str1} and {str2} are not anagrams')
         break
 else:
---
+++ corePython/Demos/DataStructure/frozensetEx/demo.py
@@ -0,0 +1,15 @@
+#1. structure: ex. frozenset({10,20,30})
+# f = frozenset({10,20,30,40})
+# f = frozenset([10,20,30,40])
+# f = frozenset({1:'a',2:'b'})
+
+#2. Heterogenous
+# f = frozenset({10,'a',3.14})
+# f = {[1,2],[3,4]}
+
+#3. Unordered
+# print(f)
+
+#4. Immutable
+# we do'nt have any method to change element.
+# we do'nt use assignment operator.
