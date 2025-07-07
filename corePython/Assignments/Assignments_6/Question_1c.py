7bad9d99ca7b77e840e1de2e91d7e6cc4c408c58
dipeshpatidarjeeran <dipeshjeeran@gmail.com>
2025-07-07 18:06:03 +0530

    add question in 6

--- corePython/Assignments/Assignments_6/Question_1c.py
+++ corePython/Assignments/Assignments_6/Question_1c.py
@@ -1,13 +1,16 @@
-list1=[]
-for i in range(1,5):
-    for j in range(1,5-i):
+n=5
+list1 =[[1]]
+for i in range(n):
+    p = list1[-1]
+    pro = [0] + p + [0]
+    new = []
+    for j in range(len(pro)-1):
+        new.append(pro[j]+pro[j+1])
+    list1.append(new)
+
+    for j in range(1,n-i):
         print(' ',end=' ')
-    k=1
-    for j in range(1,i+1):
-        if j==1 or j==i:
-            print(f'{k} ',end=' ')
-        else:
-            print('  ',end=' ')
-        
+    for j in range(i+1):
+        print(f' {list1[i][j]}',end=' ')
 
     print()
