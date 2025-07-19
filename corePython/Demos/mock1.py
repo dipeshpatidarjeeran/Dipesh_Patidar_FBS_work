0a218fffb7cd9a273794b5917d0f04893a557fc0
dipeshpatidarjeeran <dipeshjeeran@gmail.com>
2025-07-19 15:01:22 +0530

    add mock1

---
+++ corePython/Demos/mock1.py
@@ -0,0 +1,26 @@
+# n = int(input("Enter the number:-"))
+# i =2
+# while(n>i):
+#     if n%i == 0:
+#         print(f"{n} is not prime")
+#         break
+#     i+=1
+# else:
+#     print(f"{n} is prime number")
+
+
+# def person(**data):
+#     for key, value in data.items():
+#         print(f"{key}: {value}")
+
+# person(name='Ram',age=24,address='MP')
+
+
+# def fact(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * fact(n-1)
+    
+# num = int(input("enter the number:-"))
+# print("factorial is:-", fact(num))
