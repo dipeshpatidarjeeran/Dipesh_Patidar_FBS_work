4f7ae92bd5f356a72e29169f8647d14d0fef9693
dipeshpatidarjeeran <dipeshjeeran@gmail.com>
2025-07-24 21:09:46 +0530

    upate assignment 19

--- corePython/Assignments/Assignments_19/Question_Dec_1.py
+++ corePython/Assignments/Assignments_19/Question_Dec_1.py
@@ -2,3 +2,16 @@
 # calls and returns the cached result when the same inputs occur again.
 # This can greatly improve the performance of recursive or
 # computationally intensive functions.
+
+def cacheDecorator(fun):
+    def wrapper():
+        print("this is wrapper function.")
+        fun()
+    return wrapper
+
+
+@cacheDecorator
+def myFun():
+    print("this is my function")
+
+myFun()
--- corePython/Assignments/Assignments_19/Question_Gen_3.py
+++ corePython/Assignments/Assignments_19/Question_Gen_3.py
@@ -1,3 +1,40 @@
 # Write a generator function that mimics the behavior of the built-in
 # range() function. The generator should take start, stop, and step
 # arguments and yield numbers within the specified range.
+class StepZeroException(Exception):
+    def __init__(self, str1):
+        self.str1 = str1
+
+    def __str__(self):
+        return self.str1
+
+def custom_range(start, stop=None, step=1):
+    try:
+        if step == 0:
+            raise StepZeroException("step must not be Zero")
+    except StepZeroException as e:
+        return e
+    
+    if stop == None:
+        stop = start
+        start = 0
+    # print(start, stop, step)
+    # li = []
+    if step > 0:
+        while stop > start:
+            # li.append(start)
+            yield start
+            start += step
+    else:
+        while stop < start:
+            # li.append(start)
+            yield start
+            start += step
+
+    # return li
+
+c = custom_range(10,4,0)
+
+print(next(c))
+print(next(c))
+print(next(c))
