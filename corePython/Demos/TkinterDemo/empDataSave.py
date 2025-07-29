08e7ec0fed3e4c1688025e1cda0475bb1eef4ab5
dipeshpatidarjeeran <dipeshjeeran@gmail.com>
2025-07-29 17:33:21 +0530

    emp save data using tkinter

--- corePython/Demos/TkinterDemo/empDataSave.py
+++ corePython/Demos/TkinterDemo/empDataSave.py
@@ -5,10 +5,13 @@ def saveData():
     name = entry2.get()
     dept = entry3.get()
     sal = entry4.get()
-    data = f'{Id}, {name}, {dept}, {sal}'
-    with open("corePython/Demos/TkinterDemo/emp.txt", "a") as fp:
-        fp.write(data+"\n")
-        messagebox.showinfo(message="Successfully Emp Data Save")
+    if Id == '' or name == '' or dept == '' or sal == '':
+        messagebox.showerror(message="data not selected...")
+    else:
+        data = f'{Id}, {name}, {dept}, {sal}'
+        with open("corePython/Demos/TkinterDemo/emp.txt", "a") as fp:
+            fp.write(data+"\n")
+            messagebox.showinfo(message="Successfully Emp Data Save")
 
 if __name__ == "__main__":
     window = Tk()
@@ -31,10 +34,10 @@ if __name__ == "__main__":
     text2.grid(row=3, column=1)
     text3.grid(row=4, column=1)
     text4.grid(row=5, column=1)
-    entry1.grid(row=2, column=2)
-    entry2.grid(row=3, column=2)
-    entry3.grid(row=4, column=2)
-    entry4.grid(row=5, column=2)
+    entry1.grid(row=2, column=2, pady=5)
+    entry2.grid(row=3, column=2, pady=5)
+    entry3.grid(row=4, column=2, pady=5)
+    entry4.grid(row=5, column=2, pady=5)
 
-    btn.grid(row=7, column=1, columnspan=2)
+    btn.grid(row=7, column=1, columnspan=2, pady=5)
     window.mainloop()
